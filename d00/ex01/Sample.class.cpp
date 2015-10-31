0b96c7d0302a23032dba976c903ce38a7a1b1ced
Sofiane KSOUROU <sksourou@e1r11p22.42.fr>
2015-10-31 21:52:02 +0100

    yolo

---
+++ d00/ex01/Sample.class.cpp
@@ -0,0 +1,17 @@
+int main( void )
+{
+	Contact		listContact[8];
+	std::string	cmd;
+
+	while (77) {
+		std::cout << "choose a new-command[ADD - SEARCH - EXIT]" << std::endl;
+		if (std::cin == "Exit")
+				{
+					std::cout << "ah tu veux m'quitter" << std::endl;
+					return (0);
+				}
+		else if (std::cin == "ADD")
+
+
+	}
+}
